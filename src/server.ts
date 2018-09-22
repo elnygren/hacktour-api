6ff6736c308cf6732ee1053cba82d54226cdbafe
Elias Nygren <elias.nygren@outlook.com>
2018-09-22 17:06:23 +0300

    cleanup server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,28 +2,25 @@ import { IncomingMessage, ServerResponse } from 'http'
 import * as YAML from 'yamljs'
 const serve = require('serve-handler');
 
-const events = YAML.load('./src/data.yml');
+const eventsData = YAML.load('./src/data.yml');
 const scheme = process.env.NODE_ENV === 'production' ? 'https' : 'http'
+
+/** relative url to absolute url */
 const urlify = (req: IncomingMessage, relative: string) => (
   `${scheme}://${req.headers.host + relative}`)
 
+/** micro handler with routing */
 const handler = async (req: IncomingMessage, res: ServerResponse) => {
-  if (req.url && req.url.startsWith('/static/')) {
-    return serve(req, res, {public: 'public'})
-  }
-
   if (req.url && req.url.startsWith('/api/')) {
     return {
-      events: events.map((e: any) => ({...e, coverPhoto: urlify(req, e.coverPhoto)}))
+      events: eventsData.map((e: any) => ({...e, coverPhoto: urlify(req, e.coverPhoto)}))
     }
   }
 
   return serve(req, res, {
     public: 'public',
-    trailingSlash: true,
     rewrites: [{ source: '/', destination: '/index.html' }],
   })
-
-};
+}
 
 export default handler
